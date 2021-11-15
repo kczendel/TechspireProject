9238898da359c708a8e9d11899c740849240035a
Jade Nguyen <70346358+jtnguyen74@users.noreply.github.com>
2021-11-14 21:33:28 -0600

    updated

--- TechspireSite/TechspireSite/SQL/Jade N/ReportManualPointLog.sql
+++ TechspireSite/TechspireSite/SQL/Jade N/ReportManualPointLog.sql
@@ -3,7 +3,7 @@
 --The client can use this report to determine which employees have manually inputted points the most, as well as the reasons why for their actions. This is important to know for the client because it gives the client perspective in understanding if points were allocated correctly, incorrectly, due to issues with the store, or due to issues with the customers. 
 --Displays only the employees who have manually inputted points, not manually subtracted points, as well as the customer details and order numbers that are associated with the manual input of points, specifically if it is not because of an order.
 --Row Number, Employee First Name, Employee Last Name, Customer First Name, Customer Last Name, Store, Order Date, Manual Points, Reason
---,,,,,,,,
+--,,,,,,right,
 
 SELECT ROW_NUMBER() 
 
@@ -22,4 +22,5 @@ INNER JOIN Employee ON Employee.id = PointLog.employee_id
 INNER JOIN Customer ON PointLog.customer_id = Customer.id
 INNER JOIN "Order" ON PointLog.order_id = "Order".id
 INNER JOIN PointReasonType ON PointLog.reason_id = PointReasonType.id
+INNER JOIN Store ON Store.id = "Order".store_id
 WHERE PointReasonType.id <> 4
