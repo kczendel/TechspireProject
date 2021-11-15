1e6c7c2815de0e03d9e3e68342b6bae7e2507537
Jade Nguyen <70346358+jtnguyen74@users.noreply.github.com>
2021-11-14 22:33:47 -0600

    update column headings

--- TechspireSite/TechspireSite/SQL/Torrey B/ReportProductNameTotalSales.sql
+++ TechspireSite/TechspireSite/SQL/Torrey B/ReportProductNameTotalSales.sql
@@ -2,7 +2,7 @@
 --Product Name and Total Sales
 --This report will give the client insight into the total sales of a product. She can find this data useful to view which products have sold the most dollar amount overtime. The client will utilize this report during tax season or for general record keeping purposes. The reason for this is to help with financial preparations or to help the client make informed decisions about which products she offers that has the highest total sales in monetary value.
 --Included in this report is the product name and the sum sales of that product.
---Row Number, Product Name, Sum Sales 
+--Row Number, Product Name, Sum Sales, Item Price, Number of Items Sold 
 --,,right,right,right
 
 SELECT ROW_NUMBER() OVER(ORDER BY Sum(Orderline.total_price) desc) AS Num_Row,
@@ -16,5 +16,3 @@ ON OrderLine.product_id = Product.id
 INNER JOIN ProductStatus
 ON Product.product_status_id = productStatus.id
 Group By Orderline.total_price
-
-
