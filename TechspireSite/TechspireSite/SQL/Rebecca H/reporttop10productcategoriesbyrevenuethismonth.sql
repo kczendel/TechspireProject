3a761a0e49d445e5bea2fc1f6632c7798d500da8
Jade Nguyen <70346358+jtnguyen74@users.noreply.github.com>
2021-11-15 00:02:49 -0600

    typo

--- TechspireSite/TechspireSite/SQL/Rebecca H/reporttop10productcategoriesbyrevenuethismonth.sql
+++ TechspireSite/TechspireSite/SQL/Rebecca H/reporttop10productcategoriesbyrevenuethismonth.sql
@@ -1,6 +1,6 @@
 --Rebecca Hutchinson
 --Top 10 product categories by revenue this month
---TThis report is for the client to show the top 10 products categories by revenue. The client can use this to understand how her store is bringing in revenue in a way that allows her to put effort into the areas that are high ranking.
+--This report is for the client to show the top 10 products categories by revenue. The client can use this to understand how her store is bringing in revenue in a way that allows her to put effort into the areas that are high ranking.
 --Display the product category name ordered by the total revenue.
 --Product Category name, Total Revenue
 --,right
