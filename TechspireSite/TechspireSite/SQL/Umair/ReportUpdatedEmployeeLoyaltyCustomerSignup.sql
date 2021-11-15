4b63b68103b2e7d4787e35ec792b2618489ee33a
Jade Nguyen <70346358+jtnguyen74@users.noreply.github.com>
2021-11-15 00:04:57 -0600

    typo

--- TechspireSite/TechspireSite/SQL/Umair/ReportUpdatedEmployeeLoyaltyCustomerSignup.sql
+++ TechspireSite/TechspireSite/SQL/Umair/ReportUpdatedEmployeeLoyaltyCustomerSignup.sql
@@ -1,6 +1,6 @@
 --Umair Shamsuddin
 --Employee Amount of Loyalty Sign-ups In a Specific Time Period
---The client can use this report as a way to keep track of the frequency of customer sign-ups in a specific time period and what employees were most active in doing so. With this report, the client will know which employee was the most proactive in creating loyalty customers so that the performance of thiese individuals can be kept track of. For example, if the client wants to know which employees tend to get the most people to sign up as a way to reward them for their efforts, then they cn generate this report and know which name shows up the most.
+--The client can use this report as a way to keep track of the frequency of customer sign-ups in a specific time period and what employees were most active in doing so. With this report, the client will know which employee was the most proactive in creating loyalty customers so that the performance of thiese individuals can be kept track of. For example, if the client wants to know which employees tend to get the most people to sign up as a way to reward them for their efforts, then they can generate this report and know which name shows up the most.
 --This report displays an employees name, type, and status as well as the customer they signed up's first name, when they signed up, and where they signed up within a specific time frame.
 --Row number,Employee First Name,Employee Last Name,Employee Type,Employee Status,Customer First Name,Customer Begin Date,Store Name
 --,,,,,,,
