2f0fa70a919b6c3b18242ef581f56ee8d3c368ea
kczendel <77933237+kczendel@users.noreply.github.com>
2021-11-16 11:11:34 -0600

    Updated reports, replaced one

--- TechspireSite/TechspireSite/SQL/Kyle D/ReportIndividualEmployeeDuration.sql
+++ TechspireSite/TechspireSite/SQL/Kyle D/ReportIndividualEmployeeDuration.sql
@@ -1,11 +1,11 @@
 -- Kyle Dela Pena
--- Individual Employee Duration  
--- The client can see each employee and their employment duration along with their job associations, which is useful when the client wants to find a correlation between employment length and job association. For example, the first few rows display the employees who have worked with her for the longest. Inactive employees are also displayed since their employment duration will help calculate the average. Using the report, the client can identify which job associations provide the longest employee retention. However, if the client wants to find the average employment length for each job association, the Categorized Average Employee Duration report will be more useful. Since this report is for individual employees, the client can find a specific employee to compare to the average for the employee’s specific job associations. For instance, the client can find an employee who is a part-time and a cashier, then compare that specific employee’s employment length to the category’s average. She can then decide to increase a specific employee’s employment length by increasing their wage or adding bonuses, knowing that the employee is reaching the average length for their category.
--- Row Number, First Name, Last name, Duration in Days, Job Name, Type Name, Category Name, Status Name, End Date
--- null,null,null,right,null,null,null,null,null
+-- Individual Employee Duration 
+-- The client can see each employee and their employment duration along with their job associations, which is useful when the client wants to find a correlation between employment length and job association. For example, the first few rows display the employees who have worked with her for the longest. Inactive employees are also displayed since their employment duration will help calculate the average. Using the report, the client can identify which job associations provide the longest employee retention. However, if the client wants to find the average employment length for each job association, the Categorized Average Employee Duration report will be more useful. Since this report is for individual employees, the client can find a specific employee to compare the average for the specific employee job associations. For instance, the client can find an employee who is a part-time and a cashier, then compare the employment length to the average of the category. She can then decide to increase employment length by increasing their wage or adding bonuses, knowing that the employee is reaching the average length for their category.
+-- Selects all employees and the duration of employment with other employee information.
+-- First Name, Last name, Duration in Days, Job Name, Type Name, Category Name, Status Name, End Date
+-- null,null,right,null,null,null,null,null
 
-SELECT ROW_NUMBER() 
-OVER(ORDER BY Employee.last_name) AS 'Row Num', 
+SELECT 
 Employee.first_name AS 'First Name', 
 Employee.last_name AS 'Last Name', 
 DATEDIFF(day, begin_date, IIF(end_date IS NOT NULL , end_date, getdate() )) AS 'Duration in days', 
@@ -24,5 +24,4 @@ INNER JOIN EmployeeEmployeeCategory ON EmployeeEmployeeCategory.employee_id = Em
 INNER JOIN EmployeeCategory ON EmployeeEmployeeCategory.employee_category_id = EmployeeCategory.id
 
 GROUP BY Employee.first_name, Employee.last_name, Job.job_name, EmployeeType.type_name, EmployeeCategory.category_name, EmployeeStatus.status_name, Employee.end_date, Employee.begin_date
-ORDER BY 'Duration in days' DESC
-
+ORDER BY 'Duration in days' DESC, Employee.last_name
---
+++ TechspireSite/TechspireSite/SQL/Kyle D/ReportStoreRevenueByTimePeriod.sql
@@ -0,0 +1,44 @@
+-- Kyle Dela Pena
+-- Store Revenue within a given time frame
+-- The report is useful when the client wants to know which time period gives her the most revenue. Using that information, she can plan production and pricing accordingly to take advantage of the most profitable time windows. She can also limit production on days where anticipated revenue is low so that products and materials are not wasted. The client can also compare revenue between different periods, then apply her business strategies from profitable periods to mediocre or low business periods.
+-- Calculates total revenue and average revenue per order in a given time period for a given store.
+-- Time Period, Store Name, Total Orders, Products Sold, Total Revenue, Avg Revenue Per Order
+-- null,null,right,right,right,right
+
+DECLARE @store_id VARCHAR(12) = 1
+DECLARE @start_date VARCHAR(12) = '2018-08-01'
+DECLARE @end_date VARCHAR(12) = '2018-08-31'
+
+SELECT DISTINCT @start_date + ' - '+ @end_date AS 'Time Period',
+	(SELECT store_name FROM Store WHERE id = @store_id) AS 'Store Name',
+	(SELECT COUNT("Order".id) AS 'Order Count'
+	FROM "Order"
+	INNER JOIN Store ON Store.id = "Order".store_id
+	WHERE Store.id = @store_id
+	AND "Order".order_date >= @start_date AND "Order".order_date <= @end_date) AS 'Total Orders',
+	(SELECT SUM(OrderLine.quantity) AS 'Product Count'
+	FROM OrderLine
+	INNER JOIN "Order" ON "Order".id = OrderLine.order_id
+	INNER JOIN Store ON Store.id = "Order".store_id
+	WHERE Store.id = @store_id
+	AND "Order".order_date >= @start_date AND "Order".order_date <= @end_date) AS 'Products Sold',
+	(SELECT FORMAT(SUM(OrderLine.total_price),'c') AS 'OL Total Price'
+	FROM "Order"
+	INNER JOIN Store ON Store.id = "Order".store_id
+	INNER JOIN OrderLine ON OrderLine.order_id = "Order".id
+	INNER JOIN Product ON OrderLine.product_id = Product.id
+	WHERE Store.id = @store_id
+	AND "Order".order_date >= @start_date AND "Order".order_date <= @end_date) AS 'Total Revenue',
+	(SELECT FORMAT((SUM("Order".final_total) / COUNT("Order".id)),'c') AS 'Avg Revenue Per Order'
+	FROM "Order"
+	INNER JOIN Store ON Store.id = "Order".store_id
+	WHERE Store.id = @store_id
+	AND "Order".order_date >= @start_date AND "Order".order_date <= @end_date) AS 'Avg Revenue Per Order'
+
+FROM OrderLine
+INNER JOIN "Order" ON "Order".id = OrderLine.order_id
+INNER JOIN Store ON Store.id = "Order".store_id
+INNER JOIN Product ON OrderLine.product_id = Product.id
+
+
+
