12bf3fc016f77ea52d61cd392955a5d75b6fe6b6
Jade Nguyen <70346358+jtnguyen74@users.noreply.github.com>
2021-11-15 20:02:24 -0600

    Update ReportCurrentActiveEmployee.sql

--- TechspireSite/TechspireSite/SQL/Alanna G/ReportCurrentActiveEmployee.sql
+++ TechspireSite/TechspireSite/SQL/Alanna G/ReportCurrentActiveEmployee.sql
@@ -5,31 +5,19 @@
 --Row Number,First Name,Last Name,Phone Number,Job Title
 --,,,phone,
 
-SELECT ROW_NUMBER () OVER(ORDER BY Job.job_name, Employee.last_name) AS 'Row Number',
+SELECT 
 Employee.first_name AS 'First Name',
 Employee.last_name AS 'Last Name',
 Employee.phone_number AS 'Phone Number',
 Job.job_name AS 'Job Title'
 
 FROM Employee
-
-INNER JOIN EmployeeJob 
+INNER JOIN EmployeeJob
 ON EmployeeJob.employee_id = Employee.id
-
-INNER JOIN Job 
+INNER JOIN Job
 ON EmployeeJob.job_id = Job.id
-
-INNER JOIN EmployeeType
-ON Employee.employee_type_id = EmployeeType.id
-
 INNER JOIN EmployeeStatus
 ON Employee.employee_status_id = EmployeeStatus.id
 
-INNER JOIN EmployeeEmployeeCategory 
-ON EmployeeEmployeeCategory.employee_id = Employee.id
-
-INNER JOIN EmployeeCategory 
-ON EmployeeEmployeeCategory.employee_category_id = EmployeeCategory.id
-
-WHERE EmployeeStatus.id = 1 AND EmployeeJob.store_id = 1
-
+WHERE EmployeeStatus.id = 1
+ORDER BY Job.id, EmployeeJob.store_id
